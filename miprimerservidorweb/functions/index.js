4e4f1a4d292cccfca9c9ada9e58fe77b753644aa
Carla Orellana <alejandro.ledezma.orellana@gmail.com>
2022-07-15 10:42:35 -0400

    Conexion con firebase completada y funciones funcionan para llamar datos de firestore

--- miprimerservidorweb/functions/index.js
+++ miprimerservidorweb/functions/index.js
@@ -1,12 +1,14 @@
-const functions = require('firebase-functions');
-const {WebhookClient} = require('dialogflow-fulfillment');
+const functions = require('firebase-functions'); // libreria de funciones de firebase
+const {WebhookClient} = require('dialogflow-fulfillment'); // webhook para los fulfillments
 const {Card, Suggestion} = require('dialogflow-fulfillment');
 // The Firebase Admin SDK to access Firestore.
-const admin = require('firebase-admin');
+const admin = require('firebase-admin'); // necesitamos el admin de firebase
+const { Message } = require('firebase-functions/v1/pubsub');
 admin.initializeApp({
     credential: admin.credential.applicationDefault(),
     databaseURL: "https://dialogflowtestmessenger-muwf-default-rtdb.firebaseio.com/"
-});
+}); // iniciamos la app con el URL de la base de datos (de donde sacaremos los datos)
+const db = admin.firestore(); // variable para operar con los datos en firebase
 
 exports.chatbot = functions.https.onRequest((request, response) => {
     console.log("req***********: ", request.body);
@@ -14,8 +16,23 @@ exports.chatbot = functions.https.onRequest((request, response) => {
     console.log("Dialogflow Request headers: " + JSON.stringify(request.headers));
     console.log("Dialogflow Request body: " + JSON.stringify(request.body));
 
-    function Welcome(agent) {
-        agent.add(`Welcome to my agent!`);
+    function Welcome(agent) { // ejemplo para sacar datos de la table Estudiantes
+
+
+        
+        const estudiante = db.collection("Estudiantes");
+        const idEstudiante = "kw6A8oA3tQOtlAx8XKpx"; // id del estudiante
+        //
+        return estudiante.doc(idEstudiante).get().then((snapshot) => {
+            //agent.add(`Codigo: ${snapshot.id}`); // id del objeto en firebase
+            agent.add(`Codigo: ${snapshot.data().Codigo}`);
+            agent.add(`Nombre: ${snapshot.data().Nombre}`);
+            agent.add(message);
+        }).catch(() => {
+            agent.add('Puedes seleccionar otro estudiante');
+        });
+        //
+        //agent.add(`Welcome to my agent!`);
     }
 
     function fallback(agent) {
--- miprimerservidorweb/index.js
+++ miprimerservidorweb/index.js
@@ -7,72 +7,7 @@ const { WebhookClient } = require('dialogflow-fulfillment');
 const { Card, Suggestion } = require('dialogflow-fulfillment');
 
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
-///////
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCez0P1iJkZ8O3TgaLN1nbVZmnXhgO5AQ0",
-  authDomain: "dialogflowtestmessenger-muwf.firebaseapp.com",
-  projectId: "dialogflowtestmessenger-muwf",
-  storageBucket: "dialogflowtestmessenger-muwf.appspot.com",
-  messagingSenderId: "402005588569",
-  appId: "1:402005588569:web:0a078a55c7c0c7513e4f48",
-  measurementId: "G-HKCYF2PBXR"
-};
-
-admin.initializeApp(firebaseConfig);
-
-// Initialize Firebase (appfb)
-//const appfb = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(appfb);
-
-///////
-exports.chatbot = functions.https.onRequest((request, response) => {
-    console.log("req***********: ", request.body);
-    const agent = new WebhookClient({ request, response });
-    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
-
-    function welcome(agent) {
-        agent.add(`Welcome to my agent!`);
-    }
-
-    function fallback(agent) {
-        agent.add(`I didn't understand`);
-        agent.add(`I'm sorry, can you try again?`);
-    }
-
-    let intentMap = new Map();
-    intentMap.set('Saludo', Welcome); // como el default welcome intent, en el nuestro lo cambiamos a: Saludo
-    intentMap.set('Default Fallback Intent', fallback);
-    // intentMap.set('your intent name here', yourFunctionHandler);
-    // intentMap.set('your intent name here', googleAssistantHandler);
-    agent.handleRequest(intentMap);
-});
-
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-    // Grab the text parameter.
-    const original = req.query.text;
-    // Push the new message into Firestore using the Firebase Admin SDK.
-    const writeResult = await admin.firestore().collection('messages').add({ original: original });
-    // Send back a message that we've successfully written the message
-    res.json({ result: `Message with ID: ${writeResult.id} added.` });
-});
-
-exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
-    .onCreate((snap, context) => {
-        // Grab the current value of what was written to Firestore.
-        const original = snap.data().original;
-
-        // Access the parameter `{documentId}` with `context.params`
-        functions.logger.log('Uppercasing', context.params.documentId, original);
-
-        const uppercase = original.toUpperCase();
-
-        // You must return a Promise when performing asynchronous tasks inside a Functions such as
-        // writing to Firestore.
-        // Setting an 'uppercase' field in Firestore document returns a Promise.
-        return snap.ref.set({ uppercase }, { merge: true });
-    });
+admin.initializeApp();
 
 // iniciar servidor web
 app.use(express.static(__dirname + "/public"));
